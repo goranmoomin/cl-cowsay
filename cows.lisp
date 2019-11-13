2d0272e0c323be565012b21e4b1a703e2b9c85e1
Dave Cooper <david.cooper@genworks.com>
2019-11-13 08:19:42 -0500

    untabified.

--- cows.lisp
+++ cows.lisp
@@ -9,13 +9,13 @@
 (defun load-cows-from-filesystem (cowsay-path)
   (let (result)
     (dolist (file-name
-	      (directory (merge-pathnames (make-pathname :directory '(:relative "cows") :name :wild :type "cow")
-					  cowsay-path)) result)
+              (directory (merge-pathnames (make-pathname :directory '(:relative "cows") :name :wild :type "cow")
+                                          cowsay-path)) result)
       (push (cons (pathname-name file-name)
-		  (cl-ppcre:regex-replace-all "\\\\\\\\" (uiop:read-file-string file-name) "\\")) result))))
-	    
+                  (cl-ppcre:regex-replace-all "\\\\\\\\" (uiop:read-file-string file-name) "\\")) result))))
+            
 (defparameter *cows* (alexandria:alist-hash-table
-		      (load-cows-from-filesystem #.(or *compile-file-pathname* *load-pathname*)) :test 'equal))
+                      (load-cows-from-filesystem #.(or *compile-file-pathname* *load-pathname*)) :test 'equal))
 
 (defun get-cow (&key file eyes tongue thoughts)
   (let ((cow (gethash file *cows*)))
