fa67430a846d9af09553daa7683ba9f127f5786b
Dave Cooper <david.cooper@genworks.com>
2019-11-13 07:46:16 -0500

    subtle fixes
    1. fixed unbound pad function from flet
    
    2. used read-eval reader macro to make sure
       either *compile-file-pathname* or *load-pathname* is evaluated when
       needed.

--- cows.lisp
+++ cows.lisp
@@ -15,7 +15,8 @@
 		  (cl-ppcre:regex-replace-all "\\\\\\\\" (uiop:read-file-string file-name) "\\")) result))))
 	    
 (defparameter *cows* (alexandria:alist-hash-table
-		      (load-cows-from-filesystem (or *compile-file-pathname* *load-pathname*)) :test 'equal))
+		      (load-cows-from-filesystem #.(or *compile-file-pathname* *load-pathname*)) :test 'equal))
+
 
 (defun get-cow (&key file eyes tongue thoughts)
   (let ((cow (gethash file *cows*)))
