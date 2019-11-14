36b1123cdbefeb9bcde4572f992705cc7da5acaf
Sungbin Jo <pcr910303@icloud.com>
2019-11-14 23:38:10 +0900

    Update code to fix formatting

--- balloon.lisp
+++ balloon.lisp
@@ -33,19 +33,18 @@
                                      '("(" . ")"))))
     (format-balloon text wrap delimiters)))
 
-
 (defun format-balloon (text wrap delimiters)
   (flet ((split (text wrap)
            (loop :with start := 0 :while (< start (length text))
-              :for next-newline := (position #\Newline text :start (+ start 1))
-              :for wrap-at := (min (or next-newline (length text))
-                                   (+ start wrap))
-              :collect (subseq text start wrap-at)
-              :do
-                (setf start wrap-at)
-                (when (or (eql (length text) start)
-                          (eql #\Newline (char text start)))
-                  (incf start)))))
+                 :for next-newline := (position #\Newline text :start (+ start 1))
+                 :for wrap-at := (min (or next-newline (length text))
+                                      (+ start wrap))
+                 :collect (subseq text start wrap-at)
+                 :do
+                    (setf start wrap-at)
+                    (when (or (eql (length text) start)
+                              (eql #\Newline (char text start)))
+                      (incf start)))))
     (let* ((lines (if wrap (split text wrap) (list text)))
            (max-length (apply #'max (mapcar #'length lines))))
       (flet ((pad (text length) (format nil "~va" length text))
@@ -54,12 +53,13 @@
         (format nil "~{~a~^~%~}"
                 (append (list (concatenate 'string " " (top max-length)))
                         (loop :with len := (length lines)
-                           :for index :from 0 :to len
-                           :for line :in lines
-                           :for delimiter := (del-first delimiters) :then (if (/= index (- len 1))
-                                                                              (del-middle delimiters)
-                                                                              (del-last delimiters))
-                           :collect (concatenate 'string (car delimiter) " " (pad line max-length) " " (cdr delimiter)))
+                              :for index :from 0 :to len
+                              :for line :in lines
+                              :for delimiter := (del-first delimiters)
+                                :then (if (/= index (- len 1))
+                                          (del-middle delimiters)
+                                          (del-last delimiters))
+                              :collect (concatenate 'string (car delimiter) " " (pad line max-length) " " (cdr delimiter)))
                         (list (concatenate 'string " " (bottom max-length)))))))))
 
 
